f32c2d12a2fe9fac9de6b02d797d3f750f99e42a
CodeMan62 <sharmahimanshu15082007@gmail.com>
2025-01-02 10:49:35 +0530

    feat: plugins install

--- lua/theman/packer.lua
+++ lua/theman/packer.lua
@@ -8,23 +8,35 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
   use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
   })
+  use({
+    'lunarvim/horizon.nvim',
+    as = 'horizon',
+    config = function()
+      vim.cmd('colorscheme horizon')
+    end
+  })
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
   use ('nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
-	use('nvim-treesitter/playground')
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	 use {
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',  -- Ensure you use the correct branch or version
     requires = {

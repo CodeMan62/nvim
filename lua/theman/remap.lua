d4ff4aec5a21d8e08133bdc1293e0cd9b63794c5
CodeMan62 <sharmahimanshu15082007@gmail.com>
2025-01-06 10:10:22 +0530

    no arrow keys

--- lua/theman/remap.lua
+++ lua/theman/remap.lua
@@ -42,6 +42,13 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+--use nvim not the arrow keys you are neovim guy not that fuckin vscode
+vim.keymap.set("n", '<up>', '<nop>')
+vim.keymap.set("n", '<down>', '<nop>')
+vim.keymap.set("i", '<down>', '<nop>')
+vim.keymap.set("i", '<up>', '<nop>')
+vim.keymap.set("i", '<left>', '<nop>')
+vim.keymap.set("i", '<right>', '<nop>')
 
 vim.keymap.set(
     "n",
